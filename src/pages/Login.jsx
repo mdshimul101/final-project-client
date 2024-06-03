bb58c6243bbbf65c7d5f7f9d87f5781b33759ae1
mdshimul101 <shimulfjr101@gmail.com>
2024-06-03 22:09:46 +0600

    design login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,96 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import GoogleLogin from "../components/Login-Registration/GoogleLogin";
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="min-h-screen bg-gradient-to-r from-rose-100 via-gray-200 to-teal-50 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-center text-3xl font-extrabold text-gray-900 mb-4">
+          Log in
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-400 sm:text-sm"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-400 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember_me"
+                name="remember_me"
+                type="checkbox"
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label
+                htmlFor="remember_me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
+            </div>
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Log in
+            </button>
+          </div>
+          <div className="mt-6">
+            <GoogleLogin />
+          </div>
+          <div className="mt-6">
+            <p>
+              New here?{" "}
+              <Link to={"/registration"} className="text-red-500">
+                Register
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GoogleLogin from "../components/Login-Registration/GoogleLogin";
 
 const Registration = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const [passMatch, setPassMatch] = useState(true);
+
+  useEffect(() => {
+    if (password && confirmPassword && password === confirmPassword) {
+      setIsButtonDisabled(false);
+      setPassMatch(true);
+    } else {
+      setIsButtonDisabled(true);
+      setPassMatch(false);
+    }
+  }, [password, confirmPassword]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,19 +24,23 @@ const Registration = () => {
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    const password = form.password.value;
-    const confirm_password = form.confirm_password.value;
 
-    if (password !== confirm_password) {
-      setPassMatch(false);
-    }
+    console.log(name, email, password, confirmPassword);
 
-    console.log(name, email, password, confirm_password);
+    // Reset form fields
+    form.reset();
+    setPassword("");
+    setConfirmPassword("");
+    setIsButtonDisabled(true);
+    setPassMatch(true);
   };
 
-  const handleGoogleLogin = () => {
-    // Add logic for Google login here
-    console.log("Logging in with Google...");
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
   };
 
   return (
@@ -42,7 +59,7 @@ const Registration = () => {
               name="name"
               required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-400 sm:text-sm"
-              placeholder="Email address"
+              placeholder="Name"
             />
           </div>
           <div className="mb-6">
@@ -62,11 +79,12 @@ const Registration = () => {
               Password
             </label>
             <input
-              id="password"
               name="password"
               type="password"
               autoComplete="new-password"
               required
+              value={password}
+              onChange={handlePasswordChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-400 sm:text-sm"
               placeholder="Password"
             />
@@ -76,27 +94,41 @@ const Registration = () => {
               Confirm Password
             </label>
             <input
-              type="password"
               name="confirm_password"
+              type="password"
               required
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-400 sm:text-sm"
               placeholder="Confirm Password"
             />
           </div>
-          {!passMatch ? (
+          {password && confirmPassword && password !== confirmPassword ? (
             <div className="my-2">
               <p className="text-red-500">Passwords do not match!</p>
             </div>
+          ) : password && confirmPassword ? (
+            <div className="my-2">
+              <p className="text-red-500"></p>
+            </div>
           ) : (
-            ""
+            <div className="my-2">
+              <p className="text-red-500">
+                Fill up all for enable registration button
+              </p>
+            </div>
           )}
           <div className="mt-6">
             <button
               type="submit"
-              className="w-full text-white  bg-indigo-600 
-               hover:bg-indigo-700 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              disabled={isButtonDisabled}
+              className={`w-full text-white flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 ${
+                isButtonDisabled
+                  ? "bg-gray-500 cursor-not-allowed"
+                  : "bg-indigo-600 hover:bg-indigo-700"
+              }`}
             >
-              Registration
+              Register
             </button>
           </div>
           <div className="mt-4">
